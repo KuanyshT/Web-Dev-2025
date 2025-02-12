2cca0611dd52dafedcdf6c3572a66305768973ad
Kuanysh <tlenshikuanysh@gmail.com>
2025-02-12 12:14:54 +0500

    angular

--- Lab-3/Task-2/todo.js
+++ Lab-3/Task-2/todo.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const taskInput = document.getElementById("input");
     const addTaskBtn = document.getElementById("button");
     const taskList = document.getElementById("list");
+    const deleteAllButton = document.getElementById("dab");
+
 
     addTaskBtn.addEventListener("click", () => {
         const taskText = taskInput.value.trim();
+
         if (taskText) {
             const li = document.createElement("li");
-            li.innerHTML = `<span>${taskText}</span> <button class="delete">X</button>`;
+            li.innerHTML = `<span><label class = "checkbox"> <input type = "checkbox"></label></span> ${taskText} <button class="delete">X</button>`;
+
+
             li.querySelector("span").addEventListener("click", () => {
                 li.classList.toggle("completed");
             });
+
+
             li.querySelector(".delete").addEventListener("click", () => {
                 li.remove();
             });
+
+
             taskList.appendChild(li);
             taskInput.value = "";
         }
+        else{
+            alert("Empty input isnt available");
+        }
+    });
+
+    deleteAllButton.addEventListener("click", () => {
+        window.location.reload();
     });
 });
